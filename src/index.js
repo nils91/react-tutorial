27c4236f18626a1d41ab339d042cfb462eed87cd
Nils Dralle <nils.91@googlemail.com>
2020-10-25 15:23:08 +0100

    short syntax for onClick

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function(){alert('button clicked');}}>
+      <button className="square" onClick={()=>{alert('button clicked');}}>
         {this.props.value}
       </button>
     );
